091874bbdd71b70d96e92d861e8340b32b63e460
ch99AM <christianalpizar99@gmail>
2018-09-19 02:45:40 -0600

    Sobre carga de operadores completa, falta lo relacionado al ID

--- main.cpp
+++ main.cpp
@@ -3,14 +3,5 @@
 
 
 int main() {
-    MPointer<int> c = MPointer<int>();
-    MPointer<int> r = MPointer<int>();
-    c.operator=(87);
-    r.operator=(c);
-    *r = 54;
 
-
-    std::cout << "c "<< c.operator*() << std::endl;
-    std::cout << "r "<< r.operator*() << std::endl;
-    std::cout<< "&r " << r.operator&()<< std::endl;
 }
